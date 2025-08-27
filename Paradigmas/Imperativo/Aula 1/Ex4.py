31d0ac1e3afd2d368d8948a25380094feb436704
Caua Monteiro <c.monteiro1810@gmail.com>
2025-08-26 21:54:49 -0300

    Delete Paradigmas directory

--- Paradigmas/Imperativo/Aula 1/Ex4.py
+++
@@ -1,6 +0,0 @@
-a = 2
-soma = 0
-while(a < 100):
-    soma += a
-    a += 3
-print(soma)
--- Paradigmas/Imperativo/Aula 2/Ex3.py
+++
@@ -1,8 +0,0 @@
-lista = ["Homer", "Marge", "Bart", "Lisa", "Maggie"]
-
-for membro in lista:
-    soma = 0
-    for letra in membro:
-        if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
-            soma += 1
-    print(membro, " tem ", soma, " vogais")
